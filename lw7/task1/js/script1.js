7d71dda2b26c9b7430eca9905a56ffd794cad597
filippovalex7 <61573371+filippovalex7@users.noreply.github.com>
2020-04-04 10:11:53 +0300

    Add files via upload

---
+++ lw7/task1/js/script1.js
@@ -0,0 +1,26 @@
+function isPrimeNumber(number) {
+  let isPrime = true;
+  if (typeof(number) == 'number') {
+    for (let j = 2; j < number; j++) {
+      if (number % j == 0) {
+        isPrime = false
+        break
+      }
+    } 
+    if (isPrime) {
+      console.log(number + ' is prime number')
+    } else {
+      console.log(number + ' is not prime number')
+      }
+  } else {
+		if (Array.isArray(number) == false) {
+		  console.log('Incorrect type of data')
+		}
+	}
+	
+  if (Array.isArray(number)) {
+    for (let i = 0; i < number.length; i++) {
+      isPrimeNumber(number[i]);
+    }   
+  }  
+}
