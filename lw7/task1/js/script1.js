2564f31cd84e2215712d61931360f003c4785622
filippovalex7 <61573371+filippovalex7@users.noreply.github.com>
2020-05-01 13:16:49 +0300

    Add files via upload

--- lw7/task1/js/script1.js
+++ lw7/task1/js/script1.js
@@ -1,26 +1,25 @@
 function isPrimeNumber(number) {
   let isPrime = true;
   if (typeof(number) == 'number') {
+	if (number % 1 !== 0) {
+	  isPrime = false;
+	}
     for (let j = 2; j < number; j++) {
       if (number % j == 0) {
-        isPrime = false
-        break
+        isPrime = false;
+        break;
       }
     } 
     if (isPrime) {
-      console.log(number + ' is prime number')
+      console.log(number + ' is prime number');
     } else {
-      console.log(number + ' is not prime number')
+      console.log(number + ' is not prime number');
       }
+  } else if (Array.isArray(number)) {
+      for (let i = 0; i < number.length; i++) {
+        isPrimeNumber(number[i]);
+      }   
   } else {
-		if (Array.isArray(number) == false) {
-		  console.log('Incorrect type of data')
-		}
-	}
-	
-  if (Array.isArray(number)) {
-    for (let i = 0; i < number.length; i++) {
-      isPrimeNumber(number[i]);
-    }   
-  }  
+	  console.log('Incorrect type of data');
+  }
 }
